ef050eb3844048f105d986a9c2f6226a40f5e324
Jon Morton <jon.morton@gmail.com>
2015-04-24 20:35:14 -0500

    Prepare funky coastal test cases

--- test/diplomacy/core_test.clj
+++ test/diplomacy/core_test.clj
@@ -11,15 +11,12 @@
 
   (testing "Primitive geography logic"
 
-    (testing "Adjacent land"
-      (is (adjacent-land? 'Livonia 'Moscow))
-      (is (adjacent-land? 'Moscow 'Livonia))
-      (is (not (adjacent-land? 'Moscow 'Finland))))
-
-    (testing "Adjacent waters"
-      (is (adjacent-water? 'Gulf-of-Bothnia 'Baltic-Sea))
-      (is (not (adjacent-water? 'Gulf-of-Bothnia 'Barents-Sea)))
-      (is (not (adjacent-water? 'Saint-Petersburg 'Barents-Sea))))
+    (testing "Adjacency"
+      (is (adjacent? 'Livonia 'Moscow))
+      (is (adjacent? 'Moscow 'Livonia))
+      (is (adjacent? 'Livonia 'Baltic-Sea))
+      (is (adjacent? 'Baltic-Sea 'Livonia))
+      (is (not (adjacent? 'Gulf-of-Bothnia 'Moscow))))
 
     (testing "Adjacent coasts"
       ;; no special north/south coast case
@@ -39,7 +36,7 @@
 
       (testing "Coastal"
         (is (coastal? 'Saint-Petersburg))
-        (is (not (coastal? 'Russia)))))
+        (is (not (coastal? 'Russia))))))
 
   (testing "Basic Checks"
     (testing "Move to an area that is not a neighbor (6.A.1)"
@@ -63,19 +60,15 @@
         (is (valid-move? :england [:fleet 'York 'North-Sea] game))
         (is (not (valid-move? :england [:fleet 'York 'Liverpool] game)) "not coastally adjacent")
         (is (not (valid-move? :england [:fleet 'York 'English-Channel] game)) "not adjacent")
-        (is (not (valid-move? :england [:fleet 'London 'York] game))) "no fleet in london")))))
+        (is (not (valid-move? :england [:fleet 'London 'York] game))) "no fleet in london"))))
 
   (testing "Fleet attack orders"
-    ; a fleet on the north coast of StP.  cross to the Barents Sea
-    (is (valid-move? '(attack russia :fleets "Saint Petersburg" "Barents Sea")))
-    ; a fleet on the south coast of StP. cannot cross to the Barents Sea
-    (is (not (valid-move? '(attack russia :fleets "Saint Petersburg" "Barents Sea"))))
-    ; a fleet on the south coast of StP. can move to Livonia
-    (is (valid-move? '(attack russia :fleets "Saint Petersburg" "Livonia")))
-    ; a fleet on the north coast of StP. cannot cross to coastal Livonia"
-    (is (not (valid-move? '(attack russia :fleets "Saint Petersburg", "Livonia"))))))
-
-
-;; is move just attack, or does it cover convoy and support?
-
-;; Configure fleets
+    (let [game { :russia { :fleets #{ (with-meta 'Saint-Petersburg {:coast :south})}}}]
+      ;; a fleet on the north coast of StP.  cross to the Barents Sea
+      (is (valid-move? '(attack russia :fleets "Saint Petersburg" "Barents Sea")))
+      ;; a fleet on the south coast of StP. cannot cross to the Barents Sea
+      (is (not (valid-move? '(attack russia :fleets "Saint Petersburg" "Barents Sea"))))
+      ;; a fleet on the south coast of StP. can move to Livonia
+      (is (valid-move? '(attack russia :fleets "Saint Petersburg" "Livonia")))
+      ;; a fleet on the north coast of StP. cannot cross to coastal Livonia"
+      (is (not (valid-move? '(attack russia :fleets "Saint Petersburg", "Livonia")))))))
