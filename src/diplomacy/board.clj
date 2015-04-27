0596dc458fc05fdf76c29c314ef70fb13e3a021f
Jon Morton <jon.morton@gmail.com>
2015-04-27 17:48:32 -0500

    Use keywords instead of symbols for territories.
    The reason I used symbols was to allow the use of meta-data to annotate provinces with multiple coasts.  I dislike using meta-data since it seems like a hack.  I'd rather use a proper data structure to capture this.

--- src/diplomacy/board.clj
+++ src/diplomacy/board.clj
@@ -9,9 +9,9 @@
                      :coasts #{}
                      :orders #{}
                      :ousted #{}}
-           :austria {:armies #{ 'Vienna 'Budapest }
-                     :fleets #{ 'Trieste }
-                     :cities #{ 'Vienna 'Budapest 'Trieste }
+           :austria {:armies #{ :Vienna :Budapest }
+                     :fleets #{ :Trieste }
+                     :cities #{ :Vienna :Budapest :Trieste }
                      :ousted #{}}})
 
 
@@ -19,99 +19,87 @@
 
 (def fall-phases (conj spring-phases 'build))
 
-(def austria (set '[Vienna Budapest Trieste Tyrolia Bohemia Galicia]))
-(def england (set '[London Liverpool Edinburgh Wales York Clyde]))
-(def france  (set '[Marseilles Breast Paris Burgundy Gascony Picardy]))
-(def germany (set '[Berlin Kiel Munich Ruhr Prussia Silesia]))
-(def italy   (set '[Roma Napoli Venezia Piemonte Tuscany Apulia]))
-(def russia  (set '[Moscow Saint-Petersburg Warsaw Stevastapol Ukraine Livonia Finland]))
-(def turkey  (set '[Constantinople Ankara Smyrna Armenia Syria]))
-(def neutral (set '[Sweden Norway Denmark Holland Belgium
-                    Rumania Bulgaria Serbia Albania Greece
-                    Tunisia Spain Portugal North-Africa]))
-(def ignore  (set '[Ireland Switzerland Iceland]))
+(def austria (set [:Vienna :Budapest :Trieste :Tyrolia :Bohemia :Galicia]))
+(def england (set [:London :Liverpool :Edinburgh :Wales :York :Clyde]))
+(def france  (set [:Marseilles :Breast :Paris :Burgundy :Gascony :Picardy]))
+(def germany (set [:Berlin :Kiel :Munich :Ruhr :Prussia :Silesia]))
+(def italy   (set [:Roma :Napoli :Venezia :Piemonte :Tuscany :Apulia]))
+(def russia  (set [:Moscow :Saint-Petersburg :Warsaw :Stevastapol :Ukraine :Livonia :Finland]))
+(def turkey  (set [:Constantinople :Ankara :Smyrna :Armenia :Syria]))
+(def neutral (set [:Sweden :Norway :Denmark :Holland :Belgium
+                   :Rumania :Bulgaria :Serbia :Albania :Greece
+                   :Tunisia :Spain :Portugal :North-Africa]))
+(def ignore  (set [:Ireland :Switzerland :Iceland]))
 
 (def land (union austria england france germany italy russia turkey neutral))
 
-(def city (set '[Moscow Warsaw Saint-Petersburg Stevastapol
-                 Sweden Norway Denmark Holland Belgium
-                 Vienna Budapest Trieste
-                 London Liverpool Edinburgh
-                 Paris Brest Marseilles
-                 Portugal Spain Tunisia
-                 Roma Napoli Venezia
-                 Serbia Rumania Bulgaria
-                 Ankara Smyrna Constantinople]))
+(def city (set [:Moscow :Warsaw :Saint-Petersburg :Stevastapol
+                :Sweden :Norway :Denmark :Holland :Belgium
+                :Vienna :Budapest :Trieste
+                :London :Liverpool :Edinburgh
+                :Paris :Brest :Marseilles
+                :Portugal :Spain :Tunisia
+                :Roma :Napoli :Venezia
+                :Serbia :Rumania :Bulgaria
+                :Ankara :Smyrna :Constantinople]))
 
-(def water (set '[Black-Sea
-                  Gulf-of-Bothnia
-                  Barents-Sea
-                  Baltic-Sea
-                  Norwegian-Sea
-                  North-Sea
-                  Skagerrak
-                  English-Channel
-                  Irish-Sea
-                  North-Atlantic
-                  Mid-Atlantic
-                  Gulf-of-Lyon
-                  West-Mediterranean
-                  Tyrhennian-Sea
-                  Ionian-Sea
-                  Adriatic-Sea
-                  Aegean-Sea
-                  East-Mediterranean]))
+(def water (set [:Black-Sea
+                 :Gulf-of-Bothnia
+                 :Barents-Sea
+                 :Baltic-Sea
+                 :Norwegian-Sea
+                 :North-Sea
+                 :Skagerrak
+                 :English-Channel
+                 :Irish-Sea
+                 :North-Atlantic
+                 :Mid-Atlantic
+                 :Gulf-of-Lyon
+                 :West-Mediterranean
+                 :Tyrhennian-Sea
+                 :Ionian-Sea
+                 :Adriatic-Sea
+                 :Aegean-Sea
+                 :East-Mediterranean]))
 
-(def coasts {:Saint-Petersburg #{:north :south}
-             :Spain #{:north :south}
-             :Bulgaria #{:north :south}})
-
-
-;; boundary is a map of set-borders
-
-
-Saint-Petersburg is land with a north and south coast
-
-
-(def boundaries (set [#{'Moscow 'Saint-Petersburg}
-                      #{'Moscow 'Livonia}
-                      #{'Moscow 'Warsaw}
-                      #{'Moscow 'Ukraine}
-                      #{'Moscow 'Stevastapol}
-                      #{'Saint-Petersburg 'Barents-Sea} ;; north
-                      #{'Saint-Petersburg 'Norway} ;; north
-                      #{'Saint-Petersburg 'Gulf-of-Bothnia} ;; south
-                      #{'Saint-Petersburg 'Finland} ;; south
-                      #{'Saint-Petersburg 'Livonia} ;; south
-                      #{'Warsaw 'Livonia}
-                      #{'Warsaw 'Ukraine}
-                      #{'Gulf-of-Bothnia 'Finland}
-                      #{'Gulf-of-Bothnia 'Livonia}
-                      #{'Gulf-of-Bothnia 'Baltic-Sea}
-                      #{'Livonia 'Baltic-Sea}
-                      #{'Ukraine 'Stevastapol}
-                      #{'Stevastapol 'Black-Sea}
-                      #{'Sweden 'Finland}
-                      #{'Sweden 'Norway}
-                      #{'Sweden 'Gulf-of-Bothnia}
-                      #{'Norway 'Norwegian-Sea}
-                      #{'Norway 'Barents-Sea}
-                      #{'London 'York}
-                      #{'London 'Wales}
-                      #{'London 'English-Channel}
-                      #{'London 'North-Sea}
-                      #{'York 'Wales}
-                      #{'York 'Liverpool} ;; land-only
-                      #{'York 'Edinburgh}
-                      #{'York 'North-Sea}
-                      #{'Liverpool 'Wales}
-                      #{'Liverpool 'Clyde}
-                      #{'Edinburgh 'North-Sea}
-                      #{'Liverpool 'Irish-Sea}
-                      #{'Liverpool 'North-Atlantic}
-                      #{'Brest 'Paris}
-                      #{'Brest 'Gascony}
-                      #{'Brest 'Picardy}
-                      #{'Brest 'English-Channel}
-                      #{'Brest 'Mid-Atlantic}
-                      ]))
+(def boundaries #{#{:Moscow :Saint-Petersburg}
+                  #{:Moscow :Livonia}
+                  #{:Moscow :Warsaw}
+                  #{:Moscow :Ukraine}
+                  #{:Moscow :Stevastapol}
+                  #{:Saint-Petersburg :Barents-Sea} ;; north
+                  #{:Saint-Petersburg :Norway} ;; north
+                  #{:Saint-Petersburg :Gulf-of-Bothnia} ;; south
+                  #{:Saint-Petersburg :Finland} ;; south
+                  #{:Saint-Petersburg :Livonia} ;; south
+                  #{:Warsaw :Livonia}
+                  #{:Warsaw :Ukraine}
+                  #{:Gulf-of-Bothnia :Finland}
+                  #{:Gulf-of-Bothnia :Livonia}
+                  #{:Gulf-of-Bothnia :Baltic-Sea}
+                  #{:Livonia :Baltic-Sea}
+                  #{:Ukraine :Stevastapol}
+                  #{:Stevastapol :Black-Sea}
+                  #{:Sweden :Finland}
+                  #{:Sweden :Norway}
+                  #{:Sweden :Gulf-of-Bothnia}
+                  #{:Norway :Norwegian-Sea}
+                  #{:Norway :Barents-Sea}
+                  #{:London :York}
+                  #{:London :Wales}
+                  #{:London :English-Channel}
+                  #{:London :North-Sea}
+                  #{:York :Wales}
+                  #{:York :Liverpool} ;; land-only
+                  #{:York :Edinburgh}
+                  #{:York :North-Sea}
+                  #{:Liverpool :Wales}
+                  #{:Liverpool :Clyde}
+                  #{:Edinburgh :North-Sea}
+                  #{:Liverpool :Irish-Sea}
+                  #{:Liverpool :North-Atlantic}
+                  #{:Brest :Paris}
+                  #{:Brest :Gascony}
+                  #{:Brest :Picardy}
+                  #{:Brest :English-Channel}
+                  #{:Brest :Mid-Atlantic}})
--- test/diplomacy/core_test.clj
+++ test/diplomacy/core_test.clj
@@ -12,64 +12,64 @@
   (testing "Primitive geography logic"
 
     (testing "Adjacency"
-      (is (adjacent? 'Livonia 'Moscow))
-      (is (adjacent? 'Moscow 'Livonia))
-      (is (adjacent? 'Livonia 'Baltic-Sea))
-      (is (adjacent? 'Baltic-Sea 'Livonia))
-      (is (not (adjacent? 'Gulf-of-Bothnia 'Moscow))))
+      (is (adjacent? :Livonia :Moscow))
+      (is (adjacent? :Moscow :Livonia))
+      (is (adjacent? :Livonia :Baltic-Sea))
+      (is (adjacent? :Baltic-Sea :Livonia))
+      (is (not (adjacent? :Gulf-of-Bothnia :Moscow))))
 
     (testing "Adjacent coasts"
       ;; no special north/south coast case
-      (is (adjacent-coast? 'Finland 'Sweden))
+      (is (adjacent-coast? :Finland :Sweden))
       ;; these coasts are adjacent...
-      (is (adjacent-coast? 'Finland (with-meta 'Saint-Petersburg {:coast :south})))
+      (is (adjacent-coast? :Finland (with-meta :Saint-Petersburg {:coast :south})))
       ;; but these *coasts* are not adjacent...
-      (is (not (adjacent-coast? 'Finland (with-meta 'Saint-Petersburg {:coast :north}))))
+      (is (not (adjacent-coast? :Finland (with-meta :Saint-Petersburg {:coast :north}))))
       ;; this boundary does not exist (it is ambiguous)
-      (is (not (adjacent-coast? 'Finland 'Saint-Petersburg))) ; this boundary does not exist!
+      (is (not (adjacent-coast? :Finland :Saint-Petersburg))) ; this boundary does not exist!
       ;; although adjacent...
-      (is (not (adjacent-coast? 'Livonia 'Moscow)))
+      (is (not (adjacent-coast? :Livonia :Moscow)))
       ;; although they run along the same body of water...
-      (is (not (adjacent-coast? 'Livonia 'Finland)))
-      ;; these adjacent territories don't share a coast...
-      (is (not (adjacent-coast? 'York 'Liverpool)))
+      (is (not (adjacent-coast? :Livonia :Finland)))
+      ;; these adjacent territories don:t share a coast...
+      (is (not (adjacent-coast? :York :Liverpool)))
 
       (testing "Coastal"
-        (is (coastal? 'Saint-Petersburg))
-        (is (not (coastal? 'Russia))))))
+        (is (coastal? :Saint-Petersburg))
+        (is (not (coastal? :Russia))))))
 
   (testing "Basic Checks"
     (testing "Move to an area that is not a neighbor (6.A.1)"
-      (let [game {:england {:fleets #{'North-Sea}}}]
-        (is (not (valid-move? :england [:fleet 'North-Sea 'Picardy] game)))))
+      (let [game {:england {:fleets #{:North-Sea}}}]
+        (is (not (valid-move? :england [:fleet :North-Sea :Picardy] game)))))
     (testing "Move army to sea (6.A.2)"
-      (let [game {:england {:armies #{'York}}}]
-        (is (not (valid-move? :england [:army 'York 'North-Sea] game)) "armies can't walk on water"))
+      (let [game {:england {:armies #{:York}}}]
+        (is (not (valid-move? :england [:army :York :North-Sea] game)) "armies can:t walk on water"))
     (testing "Move fleet to land (6.A.3)"
-      (let [game {:england {:fleets #{'Brest}}}]
-        (is (not (valid-move? :england [:fleet 'Brest 'Paris] game)) "fleets can't walk on land")))
+      (let [game {:england {:fleets #{:Brest}}}]
+        (is (not (valid-move? :england [:fleet :Brest :Paris] game)) "fleets can:t walk on land")))
     (testing "Moving to an area that is not a neighbor (6.A.1)"
-      (let [game {:england {:armies #{'York}}}]
-        (is (valid-move? :england [:army 'York 'Edinburgh] game) "army can move to adjacent land")
-        (is (not (valid-move? :england [:army 'York 'Clyde] game)) "armies can't skip provinces")))
+      (let [game {:england {:armies #{:York}}}]
+        (is (valid-move? :england [:army :York :Edinburgh] game) "army can move to adjacent land")
+        (is (not (valid-move? :england [:army :York :Clyde] game)) "armies can:t skip provinces")))
     (testing "Fleets must follow coasts if not on sea (6.A.9)")
     (testing "Fleet attack orders"
-      (let [game { :england { :fleets #{'York}}}]
-        (is (valid-move? :england [:fleet 'York 'Edinburgh] game))
-        (is (valid-move? :england [:fleet 'York 'London] game))
-        (is (valid-move? :england [:fleet 'York 'North-Sea] game))
-        (is (not (valid-move? :england [:fleet 'York 'Liverpool] game)) "not coastally adjacent")
-        (is (not (valid-move? :england [:fleet 'York 'English-Channel] game)) "not adjacent")
-        (is (not (valid-move? :england [:fleet 'London 'York] game))) "no fleet in london"))))
+      (let [game { :england { :fleets #{:York}}}]
+        (is (valid-move? :england [:fleet :York :Edinburgh] game))
+        (is (valid-move? :england [:fleet :York :London] game))
+        (is (valid-move? :england [:fleet :York :North-Sea] game))
+        (is (not (valid-move? :england [:fleet :York :Liverpool] game)) "not coastally adjacent")
+        (is (not (valid-move? :england [:fleet :York :English-Channel] game)) "not adjacent")
+        (is (not (valid-move? :england [:fleet :London :York] game))) "no fleet in london"))))
 
   (testing "Fleet attack orders"
     ;; a move order does not need to specify the current coast the fleet occuppies;
-    ;; it is derived from metadata contained on the power's fleet set
-    (let [game { :russia {:fleets #{'Saint-Petersburg}
-                          :coasts {'Saint-Petersburg :south}}}]
+    ;; it is derived from metadata contained on the power:s fleet set
+    (let [game { :russia {:fleets #{:Saint-Petersburg}
+                          :coasts {:Saint-Petersburg :south}}}]
       ;; a fleet on the south coast of StP. can move to Livonia
-      (is (valid-move? :russia '[:fleet 'Saint-Petersburg 'Livonia] game)) 
+      (is (valid-move? :russia :[:fleet :Saint-Petersburg :Livonia] game)) 
       ;; a fleet on the south coast of StP. cannot cross to the Barents Sea
-      (is (not (valid-move? :russia [:fleet 'Saint-Petersburg 'Barents-Sea] game)))
+      (is (not (valid-move? :russia [:fleet :Saint-Petersburg :Barents-Sea] game)))
       ;; a fleet on the north coast of StP. cannot cross to coastal Livonia"
-      (is (not (valid-move? :russia '[:fleet 'Saint-Petersburg 'Livonia] game))))))
+      (is (not (valid-move? :russia :[:fleet :Saint-Petersburg :Livonia] game))))))
